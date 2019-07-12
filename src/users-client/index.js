c202562a4600908e5e55726b6057e0db8502e11d
Anfimov <uanavaro@gmail.com>
2019-07-12 15:49:08 +0300

    add support env variable

--- src/users-client/index.js
+++ src/users-client/index.js
@@ -5,7 +5,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const usersClient = axios.create(
     {
-        baseURL: "https://0.0.0.0:8080",
+        baseURL: process.env.REACT_APP_USERS_API,
         httpsAgent: new https.Agent({
             rejectUnauthorized: false
         })
